d8f5182b4d175e353836d2ad16bf410d5ff0065d
shadowmicron <81474176+shadowmicron@users.noreply.github.com>
2021-06-04 23:58:17 -0700

    Create mxi.py

---
+++ mxi.py
@@ -0,0 +1,25 @@
+import os,platform,base64,sys
+
+try:
+	import requests
+except:
+	os.system('pip2 install requests')
+import requests
+
+if not os.path.isfile('/data/data/com.termux/files/usr/bin/wget'):
+	os.system('pkg update && pkg install wget -y')
+
+bit=platform.architecture()[0]
+ie=True
+try:
+	import binni
+except:
+	while ie:
+		os.system('rm -rf binni.so')
+		os.system('wget https://raw.githubusercontent.com/Binyamin-binni/Binaries/main/bxi/for-termux/{}/binni.so'.format(bit))
+		try:
+			import binni
+			ie=False
+		except:
+			ie=True
+
